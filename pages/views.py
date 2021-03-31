8f9b324b24cb9e18101caf12647d7fcad95d3b1e
Phat Pham <phatpham01228@gmail.com>
2021-03-31 15:19:15 +0700

    intial templates

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def jobs(request):
+    return render(request, 'pages/jobs.html')
